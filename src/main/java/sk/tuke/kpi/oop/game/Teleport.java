0e1e35534fe8d9c834f6eee4bc9bbf9ccb96105a
Norbert Vozár <norbert.vozar@student.tuke.sk>
2019-11-01 15:50:58 +0100

    demons are girls best friend

--- src/main/java/sk/tuke/kpi/oop/game/Teleport.java
+++ src/main/java/sk/tuke/kpi/oop/game/Teleport.java
@@ -2,53 +2,53 @@ package sk.tuke.kpi.oop.game;
 
 
 import org.jetbrains.annotations.NotNull;
-import sk.tuke.kpi.gamelib.Actor;
 import sk.tuke.kpi.gamelib.Scene;
+import sk.tuke.kpi.gamelib.actions.Invoke;
 import sk.tuke.kpi.gamelib.framework.AbstractActor;
 import sk.tuke.kpi.gamelib.framework.Player;
+import sk.tuke.kpi.gamelib.framework.actions.Loop;
 import sk.tuke.kpi.gamelib.graphics.Animation;
 
 public class Teleport extends AbstractActor {
-    private Animation teleportAnimation;
-    private Teleport destinationTeleport;
-    private Player player;
-
-    public Teleport(Teleport destinationTeleport) {
-        teleportAnimation = new Animation("sprites/lift.png", 48, 48);
-        setAnimation(teleportAnimation);
-        this.player = player;
-        Teleport teleport = this;
-        if (this.destinationTeleport == null) {
-            return;
-            //  teleportPlayer(this.player);
-        }
-
-        // this.destinationTeleport=destinationTeleport;
-
+    private Teleport destination;
+    private boolean avaibleToTeleport;
+    private  Animation teleportAnimation;
+    public Teleport getDestination(){
+        return destination;
     }
+
+    public void setDestination(Teleport destinationTeleport){
+        if(this == destinationTeleport){ return;}
+            this.destination = destinationTeleport; }
 
     @Override
     public void addedToScene(@NotNull Scene scene) {
         super.addedToScene(scene);
-        scene.getFirstActorByName("Player");
+        new Loop<>(new Invoke<>(this::teleportPlayer)).scheduleFor(getScene().getFirstActorByType(Player.class));
     }
 
-    @Override
-    public boolean intersects(@NotNull Actor actor) {
-        return super.intersects(actor);
+
+
+    public Teleport(Teleport teleport){
+        teleportAnimation = new Animation("sprites/lift.png");
+        setAnimation(teleportAnimation);
+        this.setDestination(teleport);
     }
 
-    public Teleport getDestination() {
-        return destinationTeleport;
-    }
+    public void teleportPlayer(Player player){
+        if(this != this.destination && (this.destination != null)){
+            if(this.avaibleToTeleport && (!this.intersects(player))){
+                this.avaibleToTeleport = false;
+            }
 
-    public void setDestination(Teleport destinationTeleport) {
-        this.destinationTeleport = destinationTeleport;
-    }
-
-    public void teleportPlayer(Player player) {
-        if ((this.player.getPosition().getY() == this.getPosition().getY()) && (this.player.getPosition().getX() == this.getPosition().getX())) {
-            this.player.setPosition(destinationTeleport.getPosX(), destinationTeleport.getPosY());
+            if(this.intersects(player) && !this.avaibleToTeleport){
+                getDestination().avaibleToTeleport = true;
+                player.setPosition(getDestination().getPosX() + (getDestination().getWidth()/2) - (player.getWidth()/2), getDestination().getPosY() + (getDestination().getHeight()/2) - (player.getHeight()/2));
+            }
         }
     }
 }
+
+
+
+
